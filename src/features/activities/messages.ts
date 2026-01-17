e6c450a3bc403baad509d3ccad28a91461ae0121
Jurijs Balikovs <j.balikovs@spinwise.com>
2026-01-17 10:22:51 +0200

    fix username parse

--- src/features/activities/messages.ts
+++ src/features/activities/messages.ts
@@ -27,7 +27,7 @@ export function prepareActivityMessage({ activity, user, newLevel }: { activity:
     }
 
     return `
-      ${emojiByActivity[activityType]} *${user.username}* ${randomVerb}
+      ${emojiByActivity[activityType]} *${user.username.replaceAll('_', ' ')}* ${randomVerb}
 
       *${activityName}*
       *Продолжительность*: ${movingTime}
